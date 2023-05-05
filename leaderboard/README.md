dd1af76fe3d3a851e5b64ee2602d82875e1fb1e0
pierrbt <contact@veagle.fr>
2023-05-05 14:48:48 +0200

    Mode hors-ligne

--- leaderboard/README.md
+++ leaderboard/README.md
@@ -1,2 +1,7 @@
 # leaderboard
+
+Il faut que vous fassiez toute l'interface et le parsing des données.
+Vous utilisez HTML, CSS et JS.
+
+
 à remplir
--- leaderboard/script/leaderboard.js
+++ leaderboard/script/leaderboard.js
@@ -1,7 +1,7 @@
 
 async function Load()
 {
-    const data = await fetch("/api/list?order=score")
+    /*const data = await fetch("/api/list?order=score") // Version finale du fetch des données
         .then((res) => {
             if(!res.ok)
             {
@@ -19,10 +19,22 @@ async function Load()
         })
 
     if(!data)
-        return;
+        return;*/
+
+    const data = [ // Exemple des données qu'on recevrait avec la version production
+        {"id":6,"cps":14.9,"user":"pierrbt","date":"2023-05-03 15:50:33"},
+        {"id":7,"cps":14.6,"user":"Elie","date":"2023-05-03 15:50:33"},
+        {"id":5,"cps":14.2,"user":"Timéo","date":"2023-05-03 15:50:32"},
+        {"id":4,"cps":13.6,"user":"Oscar","date":"2023-05-03 15:50:28"},
+        {"id":2,"cps":11.4,"user":"Livet","date":"2023-05-03 15:50:27"},
+        {"id":3,"cps":10.8,"user":"Nul","date":"2023-05-03 15:50:27"},
+        {"id":1,"cps":9.4,"user":"JonCena","date":"2023-05-03 15:50:24"}
+    ];
 
     // Parse des données et affichage
 
 }
 
 Load();
+
+setInterval(Load, 5000); // Autorecharge chaque 5s
